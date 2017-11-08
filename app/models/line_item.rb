1ab3dd31687cd1dd61f17b6bc6e7cdedd0316523
Steven <steven@alkhemy.com.au>
2017-11-08 15:57:52 +1100

    completed chapter

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -7,7 +7,8 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails51 for more book information.
 #---
 class LineItem < ApplicationRecord
-  belongs_to :product
+  belongs_to :order, optional: true
+  belongs_to :product, optional: true
   belongs_to :cart
 
   def total_price
